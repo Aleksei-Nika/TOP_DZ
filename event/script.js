8f0495be433944fe1a3c8ed3da1cb3f623251f91
Aleksei-Nika <aleksei.niko42@yandex.ru>
2026-04-03 13:13:00 +0700

    Отредактировал 1 задание. Нужно добавать отмену выделение через esc. Выпяю второе задание

--- event/script.js
+++ event/script.js
@@ -15,7 +15,8 @@ listBooks.addEventListener('click', (event) => {
         if (event.ctrlKey) {
             if (targetObjs.includes(event.target)) {
                 event.target.style.backgroundColor = '#eee';
-                targetObjs.splice(targetObjs.indexOf(event.target));
+                targetObjs.splice(targetObjs.indexOf(event.target), 1);
+                console.log(targetObjs);
             } else {
                 event.target.style.backgroundColor = '#ff8026';
                 targetObjs.push(event.target);
@@ -37,21 +38,15 @@ listBooks.addEventListener('click', (event) => {
 
 document.addEventListener('keydown', (event) => {
     if (event.code == 'Delete' && targetObjs.length !=0 && document.activeElement != textarea) {
-        console.log(listBooks.children.length);
         for (obj of targetObjs) {
-            if (listBooks.children.length > 1) {
-                console.log('Удаление');
-                obj.parentElement.remove();
-            } else {
-                console.log('Освобождение');
-                obj.innerText = '';
-            }
+            obj.parentElement.remove();
         }
+        targetObjs = [];
     }
 });
 
 textarea.addEventListener('keydown', (event) => {
-    if (event.code == 'Enter' && targetObjs.length == 0) {
+    if (event.code == 'Enter' && targetObjs.length == 0 && textarea.value !== '') {
         event.preventDefault()
         let wrapperEl = document.createElement(`li`);
         let newEl = document.createElement(`p`);
@@ -67,7 +62,7 @@ textarea.addEventListener('keydown', (event) => {
         for (obj of targetObjs) {
                 obj.style.backgroundColor = `#eee`;
             }
-            targetObjs = [];
+        targetObjs = [];
     }
 });
 
@@ -75,29 +70,20 @@ textarea.addEventListener('keydown', (event) => {
 const tree = document.getElementById(`tree`);
 let focusElementTree;
 
-function mouseoverTree (event) {
-    event.target.style.fontWeight = 'bold';
-    focusElementTree = event.target;
-}
-
-tree.addEventListener('mouseover', mouseoverTree);
-
-function mouseoutTree (event) {
-    event.target.style.fontWeight = 'normal';
-    focusElementTree = null;
-}
-
-tree.addEventListener('mouseout', mouseoutTree);
-
 function clickTree (event) {
-    if (event.target.children.length != 0) {
-        if (event.target.children[0].style.display == 'none') {
-            for (obj of event.target.children) {
-                obj.style.display = 'block';
-            }
-        } else {
-            for (obj of event.target.children) {
-                obj.style.display = 'none';
+    if (event.target.tagName == 'SPAN') {
+        focusElementTree = event.target.parentElement;
+        if (focusElementTree.children.length > 1) {
+            if (focusElementTree.children[1].style.display == 'none') {
+                for (obj of focusElementTree.children) {
+                    obj.style.display = 'block';
+                }
+            } else {
+                for (obj of focusElementTree.children) {
+                    if (obj.tagName != 'SPAN') {
+                        obj.style.display = 'none'
+                    }
+                }
             }
         }
     }
@@ -112,10 +98,7 @@ function closeMenu(event) {
     if (event.target !== menu) {
         menu.style.display = 'none';
         document.removeEventListener('click', closeMenu);
-        tree.addEventListener('mouseover', mouseoverTree);
-        tree.addEventListener('mouseout', mouseoutTree);
-        tree.addEventListener('click', clickTree);
-        focusElementTree.style.fontWeight = 'normal';
+        // focusElementTree.style.fontWeight = 'normal';
         flagMenu = false;
     }
 }
@@ -124,36 +107,32 @@ tree.addEventListener('contextmenu', (event) =>{
     event.preventDefault();
     if (!flagMenu) {
         document.addEventListener('click', closeMenu);
-        tree.removeEventListener('mouseover', mouseoverTree);
-        tree.removeEventListener('mouseout', mouseoutTree);
-        tree.removeEventListener('click', clickTree);
     }
     flagMenu = true;
-    object = event.target;
     menu.style.display = 'flex';
     menu.style.left = `${event.clientX}px`;
     menu.style.top = `${event.clientY}px`;
 });
 
 document.getElementById('menu-add').addEventListener('click', (event) => {
-    console.log(focusElementTree.children);
+    console.log(focusElementTree);
 });
 
-document.getElementById('menu-addChild').addEventListener('click', (event) => {
+// document.getElementById('menu-addChild').addEventListener('click', (event) => {
     
-});
+// });
 
-document.getElementById('menu-rename').addEventListener('click', (event) => {
-    if (focusElementTree.children.length === 0) {
-        let edit = document.createElement('textarea');
-        edit.value = focusElementTree.innerText;
-        edit.rows = 1;
-        edit.cols = 50;
-        focusElementTree.replaceWith(edit);
-    }
-    focusElementTree.innerHTML = document.createElement('textarea');
-});
+// document.getElementById('menu-rename').addEventListener('click', (event) => {
+//     if (focusElementTree.children.length === 0) {
+//         let edit = document.createElement('textarea');
+//         edit.value = focusElementTree.innerText;
+//         edit.rows = 1;
+//         edit.cols = 50;
+//         focusElementTree.replaceWith(edit);
+//     }
+//     focusElementTree.innerHTML = document.createElement('textarea');
+// });
 
-document.getElementById('menu-del').addEventListener('click', (event) => {
-    focusElementTree.remove();
-});
+// document.getElementById('menu-del').addEventListener('click', (event) => {
+//     focusElementTree.remove();
+// });
